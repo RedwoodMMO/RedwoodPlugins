0280b1995790be6fc0bf62af89a022e13ec705d0
Mike Seese <seesemichaelj@gmail.com>
2025-01-30 17:14:32 -0800

    fix issue with lyra's game mode structure

--- RedwoodCore/Source/Redwood/Private/RedwoodGameModeBase.cpp
+++ RedwoodCore/Source/Redwood/Private/RedwoodGameModeBase.cpp
@@ -19,10 +19,17 @@ void REDWOOD_GAME_MODE_TYPE::InitGame(
 ) {
   Super::InitGame(MapName, Options, ErrorMessage);
 
-  GameModeComponent->InitVariables(
-    DatabasePersistenceInterval, PostBeginPlayDelay
-  );
-  GameModeComponent->InitGame(MapName, Options, ErrorMessage);
+  UWorld *World = GetWorld();
+
+  if (IsValid(World) && (
+      World->GetNetMode() == ENetMode::NM_DedicatedServer ||
+      World->GetNetMode() == ENetMode::NM_ListenServer
+    )) {
+    GameModeComponent->InitVariables(
+      DatabasePersistenceInterval, PostBeginPlayDelay
+    );
+    GameModeComponent->InitGame(MapName, Options, ErrorMessage);
+  }
 }
 
 APlayerController *REDWOOD_GAME_MODE_TYPE::Login(
@@ -33,75 +40,116 @@ APlayerController *REDWOOD_GAME_MODE_TYPE::Login(
   const FUniqueNetIdRepl &UniqueId,
   FString &ErrorMessage
 ) {
-  return GameModeComponent->Login(
-    NewPlayer,
-    InRemoteRole,
-    Portal,
-    Options,
-    UniqueId,
-    ErrorMessage,
-    std::function<APlayerController
-                    *(UPlayer *,
-                      ENetRole,
-                      const FString &,
-                      const FString &,
-                      const FUniqueNetIdRepl &,
-                      FString &)>(
-      [this](
-        UPlayer *DelegateNewPlayer,
-        ENetRole DelegateInRemoteRole,
-        const FString &DelegatePortal,
-        const FString &DelegateOptions,
-        const FUniqueNetIdRepl &DelegateUniqueId,
-        FString &DelegateErrorMessage
-      ) -> APlayerController * {
-        return Super::Login(
-          DelegateNewPlayer,
-          DelegateInRemoteRole,
-          DelegatePortal,
-          DelegateOptions,
-          DelegateUniqueId,
-          DelegateErrorMessage
-        );
-      }
-    )
-  );
+  UWorld *World = GetWorld();
+
+  if (IsValid(World) && (
+      World->GetNetMode() == ENetMode::NM_DedicatedServer ||
+      World->GetNetMode() == ENetMode::NM_ListenServer
+    )) {
+    return GameModeComponent->Login(
+      NewPlayer,
+      InRemoteRole,
+      Portal,
+      Options,
+      UniqueId,
+      ErrorMessage,
+      std::function<APlayerController
+                      *(UPlayer *,
+                        ENetRole,
+                        const FString &,
+                        const FString &,
+                        const FUniqueNetIdRepl &,
+                        FString &)>(
+        [this](
+          UPlayer *DelegateNewPlayer,
+          ENetRole DelegateInRemoteRole,
+          const FString &DelegatePortal,
+          const FString &DelegateOptions,
+          const FUniqueNetIdRepl &DelegateUniqueId,
+          FString &DelegateErrorMessage
+        ) -> APlayerController * {
+          return Super::Login(
+            DelegateNewPlayer,
+            DelegateInRemoteRole,
+            DelegatePortal,
+            DelegateOptions,
+            DelegateUniqueId,
+            DelegateErrorMessage
+          );
+        }
+      )
+    );
+  } else {
+    return Super::Login(
+      NewPlayer, InRemoteRole, Portal, Options, UniqueId, ErrorMessage
+    );
+  }
 }
 
 bool REDWOOD_GAME_MODE_TYPE::PlayerCanRestart_Implementation(
   APlayerController *Player
 ) {
-  return GameModeComponent->PlayerCanRestart_Implementation(
-    Player,
-    std::function<bool(APlayerController *)>(
-      [this](APlayerController *DelegatePlayer) -> bool {
-        return Super::PlayerCanRestart_Implementation(DelegatePlayer);
-      }
-    )
-  );
+  UWorld *World = GetWorld();
+
+  if (IsValid(World) && (
+      World->GetNetMode() == ENetMode::NM_DedicatedServer ||
+      World->GetNetMode() == ENetMode::NM_ListenServer
+    )) {
+    return GameModeComponent->PlayerCanRestart_Implementation(
+      Player,
+      std::function<bool(APlayerController *)>(
+        [this](APlayerController *DelegatePlayer) -> bool {
+          return Super::PlayerCanRestart_Implementation(DelegatePlayer);
+        }
+      )
+    );
+  } else {
+    return Super::PlayerCanRestart_Implementation(Player);
+  }
 }
 
 void REDWOOD_GAME_MODE_TYPE::FinishRestartPlayer(
   AController *NewPlayer, const FRotator &StartRotation
 ) {
-  GameModeComponent->FinishRestartPlayer(
-    NewPlayer,
-    StartRotation,
-    std::function<void(AController *)>([this](AController *DelegateNewPlayer) {
-      FailedToRestartPlayer(DelegateNewPlayer);
-    })
-  );
+  UWorld *World = GetWorld();
+
+  if (IsValid(World) && (
+      World->GetNetMode() == ENetMode::NM_DedicatedServer ||
+      World->GetNetMode() == ENetMode::NM_ListenServer
+    )) {
+    GameModeComponent->FinishRestartPlayer(
+      NewPlayer,
+      StartRotation,
+      std::function<void(AController *)>([this](AController *DelegateNewPlayer
+                                         ) {
+        FailedToRestartPlayer(DelegateNewPlayer);
+      })
+    );
+  } else {
+    Super::FinishRestartPlayer(NewPlayer, StartRotation);
+  }
 }
 
 APawn *REDWOOD_GAME_MODE_TYPE::SpawnDefaultPawnAtTransform_Implementation(
   AController *NewPlayer, const FTransform &SpawnTransform
 ) {
-  FTransform NewSpawnTransform =
-    GameModeComponent->PickPawnSpawnTransform(NewPlayer, SpawnTransform);
+  UWorld *World = GetWorld();
 
-  return Super::SpawnDefaultPawnAtTransform_Implementation(
-    NewPlayer, NewSpawnTransform
-  );
+  if (IsValid(World) && (
+      World->GetNetMode() == ENetMode::NM_DedicatedServer ||
+      World->GetNetMode() == ENetMode::NM_ListenServer
+    )) {
+    FTransform NewSpawnTransform =
+      GameModeComponent->PickPawnSpawnTransform(NewPlayer, SpawnTransform);
+
+    return Super::SpawnDefaultPawnAtTransform_Implementation(
+      NewPlayer, NewSpawnTransform
+    );
+  } else {
+    return Super::SpawnDefaultPawnAtTransform_Implementation(
+      NewPlayer, SpawnTransform
+    );
+  }
 }
 
 #undef REDWOOD_GAME_MODE_TYPE
